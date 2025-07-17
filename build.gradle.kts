1acedf242a15714c96c5b164fbdff273213d19f8
kdmstj <rkddmstj429@gmail.com>
2025-07-17 16:24:18 +0900

    chore: OpenAPI swagger 설정
    - 의존성 추가
    - Configuration 설정 파일로 API 문서 제목 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
