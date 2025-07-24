ca2d838ca98791ebb02d9bc811cbcd90a3983689
kdmstj <rkddmstj429@gmail.com>
2025-07-24 12:01:00 +0900

    test: product 상세/목록 조회
    - 조회 시 역직렬화 검증
    - 예외 발생 시 공통 에러 처리 검증
    - 중복 코드를 제거하기 위해서 Fixture 생성

---
+++ src/test/java/kr/hhplus/be/server/product/fixture/ProductFixture.java
@@ -0,0 +1,35 @@
+package kr.hhplus.be.server.product.fixture;
+
+import kr.hhplus.be.server.product.domain.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductFixture {
+    public static List<Product> createList(int n) {
+        List<Product> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            Product product = withIdAndProductName(i, "product" + i);
+            list.add(product);
+        }
+        return list;
+    }
+
+    public static Product withId(long id){
+        return Product.builder()
+                .id(id)
+                .productName("product")
+                .pricePerUnit(1000)
+                .quantity(100)
+                .build();
+    }
+
+    public static Product withIdAndProductName(long id, String productName){
+        return Product.builder()
+                .id(id)
+                .productName(productName)
+                .pricePerUnit(1000)
+                .quantity(100)
+                .build();
+    }
+}
