b36d45986559deaa9b82abd28d44e2b7bb922a6d
kdmstj <rkddmstj429@gmail.com>
2025-07-31 10:59:19 +0900

    test: 쿠폰 발급 메서드 서비스 통합 테스트
    - 코드 중복도를 낮추기 위해서 유효한 기간을 가진 Coupon 생성하는 CouponFixture 생성

--- src/test/java/kr/hhplus/be/server/coupon/application/service/CouponServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/coupon/application/service/CouponServiceIntegrationTest.java
@@ -1,13 +1,18 @@
 package kr.hhplus.be.server.coupon.application.service;
 
 import kr.hhplus.be.server.DataBaseCleanUp;
+import kr.hhplus.be.server.common.BusinessException;
+import kr.hhplus.be.server.common.ErrorCode;
 import kr.hhplus.be.server.coupon.application.result.UserCouponResult;
 import kr.hhplus.be.server.coupon.domain.UserCouponStatus;
+import kr.hhplus.be.server.coupon.domain.entity.Coupon;
 import kr.hhplus.be.server.coupon.domain.entity.UserCoupon;
 import kr.hhplus.be.server.coupon.domain.repository.CouponQuantityRepository;
 import kr.hhplus.be.server.coupon.domain.repository.CouponRepository;
 import kr.hhplus.be.server.coupon.domain.repository.UserCouponRepository;
 import kr.hhplus.be.server.coupon.domain.repository.UserCouponStateRepository;
+import kr.hhplus.be.server.coupon.fixture.CouponFixture;
+import kr.hhplus.be.server.coupon.fixture.CouponQuantityFixture;
 import kr.hhplus.be.server.coupon.fixture.UserCouponFixture;
 import kr.hhplus.be.server.coupon.fixture.UserCouponStateFixture;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,9 +23,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Testcontainers
@@ -79,6 +86,107 @@ public class CouponServiceIntegrationTest {
             assertThat(result)
                     .extracting(UserCouponResult::couponId)
                     .doesNotContain(11L, 12L);
+        }
+
+        @Nested
+        @DisplayName("쿠폰 발급")
+        class IssueUserCoupon {
+
+            @Test
+            @DisplayName("성공")
+            void 쿠폰_발급_성공(){
+                //given
+                Coupon coupon = couponRepository.save(CouponFixture.validPeriod());
+                int issuedQuantity = 0;
+                couponQuantityRepository.save(CouponQuantityFixture.withCouponIdAndTotalQuantityAndIssuedQuantity(coupon.getId(), 100, issuedQuantity));
+                long userId = 1L;
+
+                //when
+                couponService.issue(userId, coupon.getId());
+
+                //then
+                assertThat(couponQuantityRepository.findOneByCouponId(coupon.getId()).getIssuedQuantity()).isEqualTo(issuedQuantity + 1);
+                assertThat(userCouponRepository.existsByUserIdAndCouponId(userId, coupon.getId())).isTrue();
+            }
+
+            @Test
+            @DisplayName("실패 - 이미 보유한 쿠폰")
+            void 쿠폰_발급_실패_이미_보유한_쿠폰(){
+                //given
+                Coupon coupon = couponRepository.save(CouponFixture.validPeriod());
+                couponQuantityRepository.save(CouponQuantityFixture.withCouponIdAndTotalQuantityAndIssuedQuantity(coupon.getId(), 100, 1));
+                long userId = 1L;
+                userCouponRepository.save(UserCouponFixture.withUserIdAndCouponId(userId, coupon.getId()));
+
+                // when & then
+                assertThatThrownBy(() -> couponService.issue(userId, coupon.getId()))
+                        .isInstanceOf(BusinessException.class)
+                        .hasMessageContaining(ErrorCode.ALREADY_ISSUED_COUPON.getMessage());
+                assertThat(userCouponRepository.existsByUserIdAndCouponId(userId, coupon.getId())).isTrue();
+                assertThat(couponQuantityRepository.findOneByCouponId(coupon.getId()).getIssuedQuantity()).isEqualTo(1);
+            }
+
+            @Test
+            @DisplayName("실패 - 존재하지 않는 쿠폰")
+            void 쿠폰_발급_실패_존재하지_않는_쿠폰(){
+                // when & then
+                long userId = 1L;
+                assertThatThrownBy(() -> couponService.issue(1L, 999L))
+                        .isInstanceOf(BusinessException.class)
+                        .hasMessageContaining(ErrorCode.COUPON_NOT_FOUND.getMessage());
+
+                assertThat(userCouponRepository.existsByUserIdAndCouponId(userId, 999L)).isFalse();
+            }
+
+            @Test
+            @DisplayName("실패 - 발급 시작 기간 이전인 쿠폰")
+            void 쿠폰_발급_실패_시작_기간이_이전_쿠폰(){
+                //given
+                Coupon coupon = couponRepository.save(CouponFixture.withIssuedStartedAtAndIssuedEndedAt(LocalDateTime.now().plusDays(1), LocalDateTime.now().plusWeeks(1)));
+                couponQuantityRepository.save(CouponQuantityFixture.withCouponIdAndTotalQuantityAndIssuedQuantity(coupon.getId(), 100, 0));
+                long userId = 1L;
+
+                // when & then
+                assertThatThrownBy(() -> couponService.issue(userId, coupon.getId()))
+                        .isInstanceOf(BusinessException.class)
+                        .hasMessageContaining(ErrorCode.ISSUE_PERIOD_NOT_STARTED.getMessage());
+                assertThat(userCouponRepository.existsByUserIdAndCouponId(userId, coupon.getId())).isFalse();
+                assertThat(couponQuantityRepository.findOneByCouponId(coupon.getId()).getIssuedQuantity()).isEqualTo(0);
+            }
+
+            @Test
+            @DisplayName("실패 - 발급 기간이 지난 쿠폰")
+            void 쿠폰_발급_실패_발급_기간이_지난_쿠폰(){
+                //given
+                Coupon coupon = couponRepository.save(CouponFixture.withIssuedStartedAtAndIssuedEndedAt(LocalDateTime.now().minusWeeks(1), LocalDateTime.now().minusDays(1)));
+                couponQuantityRepository.save(CouponQuantityFixture.withCouponIdAndTotalQuantityAndIssuedQuantity(coupon.getId(), 100, 0));
+                long userId = 1L;
+
+                // when & then
+                assertThatThrownBy(() -> couponService.issue(userId, coupon.getId()))
+                        .isInstanceOf(BusinessException.class)
+                        .hasMessageContaining(ErrorCode.ISSUE_PERIOD_ENDED.getMessage());
+                assertThat(userCouponRepository.existsByUserIdAndCouponId(userId, coupon.getId())).isFalse();
+                assertThat(couponQuantityRepository.findOneByCouponId(coupon.getId()).getIssuedQuantity()).isEqualTo(0);
+            }
+
+            @Test
+            @DisplayName("실패 - 수량이 초과한 쿠폰")
+            void 쿠포_발급_실패_수량이_초과한_쿠폰(){
+                //given
+                Coupon coupon = couponRepository.save(CouponFixture.validPeriod());
+                int totalQuantity = 100; int issuedQuantity = 100;
+                couponQuantityRepository.save(CouponQuantityFixture.withCouponIdAndTotalQuantityAndIssuedQuantity(coupon.getId(), totalQuantity, issuedQuantity));
+                long userId = 1L;
+
+                // when & then
+                assertThatThrownBy(() -> couponService.issue(userId, coupon.getId()))
+                        .isInstanceOf(BusinessException.class)
+                        .hasMessageContaining(ErrorCode.EXCEED_QUANTITY.getMessage());
+                assertThat(userCouponRepository.existsByUserIdAndCouponId(userId, coupon.getId())).isFalse();
+                assertThat(couponQuantityRepository.findOneByCouponId(coupon.getId()).getIssuedQuantity()).isEqualTo(issuedQuantity);
+
+            }
         }
     }
 }
--- src/test/java/kr/hhplus/be/server/coupon/fixture/CouponFixture.java
+++ src/test/java/kr/hhplus/be/server/coupon/fixture/CouponFixture.java
@@ -17,4 +17,15 @@ public class CouponFixture {
                 .createdAt(LocalDateTime.now())
                 .build();
     }
+
+    public static Coupon validPeriod(){
+        return Coupon.builder()
+                .name("coupon")
+                .discountType(DiscountType.FIXED)
+                .discountValue(10000)
+                .issuedStartedAt(LocalDateTime.now().minusWeeks(1))
+                .issuedEndedAt(LocalDateTime.now().plusWeeks(1))
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 }
