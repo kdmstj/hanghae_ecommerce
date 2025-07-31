2c7ab16eb768fe87e370412a179b4dd2c1c821c5
kdmstj <rkddmstj429@gmail.com>
2025-07-31 11:58:11 +0900

    refactor(test): 포인트 충전 service 단위 테스트 삭제
    - 포인트 충전 service 통합 테스트를 새로 생성하면서 단위 테스트는 무의미하다고 판단하여 삭제하였습니다.

--- src/test/java/kr/hhplus/be/server/point/application/service/PointServiceTest.java
+++ src/test/java/kr/hhplus/be/server/point/application/service/PointServiceTest.java
@@ -1,6 +1,5 @@
 package kr.hhplus.be.server.point.application.service;
 
-import kr.hhplus.be.server.point.application.command.PointChargeCommand;
 import kr.hhplus.be.server.point.application.command.PointUseCommand;
 import kr.hhplus.be.server.point.domain.entity.PointHistory;
 import kr.hhplus.be.server.point.domain.entity.UserPoint;
@@ -33,31 +32,6 @@ public class PointServiceTest {
 
     @InjectMocks
     PointService pointService;
-
-    @Nested
-    @DisplayName("포인트 충전")
-    class ChargePoint {
-
-        @Test
-        @DisplayName("포인트 충전 시 유저의 포인트가 증가하고 이력이 기록된다")
-        void 포인트_충전_검증() {
-            // given
-            long userId = 1L;
-            int chargeAmount = 1_000;
-            UserPoint current = UserPointFixture.withUserIdAndBalance(userId, 5_000);
-
-            given(userPointRepository.findOneByUserId(userId))
-                    .willReturn(Optional.of(current));
-
-            PointChargeCommand command = new PointChargeCommand(userId, chargeAmount);
-
-            // when
-            pointService.charge(command);
-
-            // then
-            verify(pointHistoryRepository, times(1)).save(any(PointHistory.class));
-        }
-    }
 
     @Nested
     @DisplayName("포인트 사용")
