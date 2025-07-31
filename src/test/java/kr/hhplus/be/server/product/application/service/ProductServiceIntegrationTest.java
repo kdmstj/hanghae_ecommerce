4fddf3dc3ca40f7e55632a06b871ef956cf22c13
kdmstj <rkddmstj429@gmail.com>
2025-07-31 16:26:43 +0900

    test: 쿠폰 사용 서비스 통합 테스트
    - 사용 목록 중 하나라도 실패한다면 모두 롤백되는지 확인

---
+++ src/test/java/kr/hhplus/be/server/product/application/service/ProductServiceIntegrationTest.java
@@ -0,0 +1,9 @@
+package kr.hhplus.be.server.product.application.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public class ProductServiceIntegrationTest {
+}
