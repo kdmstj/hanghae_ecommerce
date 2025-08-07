0722be22960e10193220e88108605b6519075e9b
kdmstj <rkddmstj429@gmail.com>
2025-08-08 08:54:01 +0900

    refactor: 상품 차감 비관적 락 적용
    - 재고 감소 시 업데이트 날짜 업데이트

--- src/main/java/kr/hhplus/be/server/product/domain/repository/ProductRepository.java
+++ src/main/java/kr/hhplus/be/server/product/domain/repository/ProductRepository.java
@@ -1,7 +1,15 @@
 package kr.hhplus.be.server.product.domain.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.product.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Product p where p.id = :id")
+    Optional<Product> findWithPessimisticLock(long id);
 }
